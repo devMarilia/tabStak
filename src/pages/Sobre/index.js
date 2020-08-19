6e1310854a422c13c232237bfaa583db3173d59b
devMarilia <marilia.lemos753@gmail.com>
2020-08-19 16:22:05 -0300

    Criado componente básico  Home/Sobre/Contato

---
+++ src/pages/Sobre/index.js
@@ -0,0 +1,10 @@
+import Reaxr from 'react'
+import { View, Text, StyleSheet} from 'react-native'
+
+export default function Sobre(){
+    return(
+        <View>
+            <Text>Contato</Text>
+        </View>
+    )
+}
